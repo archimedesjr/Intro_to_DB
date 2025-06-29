72e1bdc5ef5ce777a411f6fbf6055774210c4674
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-06-29 21:25:09 +0100

    Add task_6.sql to Intro_to_DB

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,6 @@
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'),
-      (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
       (3, 'Nehemial Kamolu', 'eobed@sandtech.com', '125 Happiness  Ave.'),
-      (4, 'Cole Baidoo', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+      (4, 'Cole Baidoo', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
